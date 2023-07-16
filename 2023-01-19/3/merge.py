0c7eb9b9c2a0bbc53ba1d98ebfb2e9c3417ad192
papum20_lenovo <danieledugo1@gmail.com>
2023-07-16 18:23:20 +0200

    done 2023 01 19/3

--- 2023-01-19/3/merge.py
+++ 2023-01-19/3/merge.py
@@ -6,19 +6,32 @@ def copy_all(src, dst):
     dst = dst.strip('/')
 
     for path, dirs, files in os.walk(src):
-        rel_path = path[(path.find(src) + len(src):]
+        if path.find(f'{src}/') != -1:
+            rel_path = path[(path.find(f'{src}/') + len(src)):]
+        elif path.strip('.').strip('/') == src:
+            rel_path = ''
+        else:
+            rel_path = path.strip('.')
+        rel_path = rel_path.strip('/')
+        print(f'rel path: {rel_path}')
         # mk missing dirs
         for d in dirs:
+            print(f'dir: {d}')
             p = os.path.join(dst, rel_path, d)
+            print(f'dir newpath: {p}')
             if not os.path.exists(p):
                 os.mkdir(p)
         # copy files append
         for file in files:
+            print(f'file: {file}')
             # read
             with open(os.path.join(path, file), "r") as fr:
                 # write
-                with open(os.path.join(dst, rel_path, file), "a") as fw:
-                    
+                p = (os.path.join(dst, rel_path, file))
+                print(f'p for write: {p}')
+                with open(p, "a") as fw:
+                    for line in fr.readlines():
+                        fw.write(line)
 
 
 
@@ -27,5 +40,7 @@ if len(sys.argv) < 4:
     exit(1)
 
 a, b, c = sys.argv[1:4]
+print(a,b,c)
 
-for 
+copy_all(a, c)
+copy_all(b, c)
