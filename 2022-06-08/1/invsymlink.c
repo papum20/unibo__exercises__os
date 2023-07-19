d0f8cb953dbde60fdcb0de6ea83ca50844ba6418
pippo_vagrant <danieledugo1@gmail.com>
2023-07-19 18:03:08 +0000

    2022 06 08 not done

---
+++ 2022-06-08/1/invsymlink.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define SIZE 200
+
+int main(int argc, char *argv[]) {
+
+	char path[SIZE];
+	int res;
+	
+
+	realpath(argv[1], path);
+
+
+	
+
+
+
+}
